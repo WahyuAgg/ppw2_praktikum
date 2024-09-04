92de08504905318c1587379bd94dfb71f2ec4e97
WahyuAgg <wahyuagungbarokah@mail.ugm.ac.id>
2024-09-04 09:25:04 +0700

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.template')
+
+@section('title', 'Home') <!-- Menetapkan judul untuk halaman ini menjadi 'Home' -->
+
+@section('content')
+    <!-- Menetapkan isi konten untuk bagian @yield('content') di file template  -->
+    <!--  -->
+    <h2>Selamat datang di Halaman Utama</h2>
+    <!-- Tabel menampilkan data pengguna -->
+    <table>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Nama</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Wahyu</td>
+                <td>wahyu@example.com</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Agung</td>
+                <td>agung@example.com</td>
+            </tr>
+        </tbody>
+    </table>
+@endsection
---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Menetapkan judul halaman. Default ke 'Default Title' jika tidak ditentukan dari home.blade.php-->
+    <title>@yield('title', 'Default Title')</title>
+    
+    <!-- Menautkan file'css' -->
+    <link rel="stylesheet" href="{{ asset('css/style_home.css') }}">
+</head>
+<body>
+    <!-- Bagian header, judul situs dan menu navigasi -->
+    <header>
+        <h1>My Laravel Application</h1>
+        <nav>
+            <ul>
+                <li><a href="{{ url('/') }}">Home</a></li>
+                <li><a href="{{ url('/about') }}">About</a></li>
+                <li><a href="{{ url('/contact') }}">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <!-- Kontainer untuk konten utama halaman -->
+    <div class="container">
+        @yield('content') 
+        <!-- Ini akan digantikan oleh konten dari home.blade.php -->
+    </div>
+
+    <!-- Bagian footer -->
+    <footer>
+        <p>&copy; 2024 Laravel</p>
+    </footer>
+</body>
+</html>
